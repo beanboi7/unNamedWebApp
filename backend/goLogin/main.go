343cd7cfcbf5a7d6f6b08415cf4fd2e91aa329ba
Sriram J <srirocksplayer@gmail.com>
2021-05-19 22:52:44 +0530

    go api for login

---
+++ backend/goLogin/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"goLogin/controller"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/register", controller.RegisterHandler).Methods("POST")
+	r.HandleFunc("/login", controller.LoginHandler).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
--- frontend/src/components/RegisterForm/RegisterationForm.jsx
+++ frontend/src/components/RegisterForm/RegisterationForm.jsx
@@ -1,39 +1,67 @@
 import React, { useState } from 'react';
+import AlertComponent from "../Alert/AlertComponent";
 
-function RegisterationForm(){
+
+Need to add success MessageChannel, server status, redirection to home
+
+
+function RegisterationForm(props){
     const [currentState, setState] = useState({
             email : "",
-            password : ""
+            password : "",
+            confirmpassword : "",
         });
     
-    const handleChange = e => {
+    const handleChange = (e) => {
         const {id,value} = e.target;
         setState(currentState => ({
             ...currentState,
             [id]: value,
         }))
     }
+    
+    const handleSubmitChange = (e) =>{
+        e.preventDefault();
+        if(currentState.password === currentState.confirmpassword){
+            //call api and handle it
+            console.log("call the backend API to post the registration details")
+        } else{
+            props.showError("Passwords don't match!")
+        }
+    }
 
     
     return(
-        <div>
+        <div className ="card col-12 col-lg-4 login-card mt-2 hv-center">
+        <AlertComponent />
             <form> 
-                <div className="form-group text-left">
+                <div className="col-auto">
                     <label htmlFor="exampleInputEmail1">Email address</label>
                     <input type = "email"
                         className = "form-control"
                         id = "email"
                         aria-describedby = "emailHelp"
-                        placeholder = "Enter your email" />
+                        placeholder = "Enter your email" 
+                        onChange = {handleChange}
+                        />
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
 
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputEmail1">Password</label>
-                    <input type = "password"
-                        className = "form-control"
-                        id = "password"
-                        placeholder = "Enter your password" />
+                <div className="col-auto">
+                    <label for="inputPassword6" class="col-form-label">Password</label>
+                </div>
+                <div className="col-auto">
+                    <input type="password" 
+                    id="inputPassword6" 
+                    class="form-control" 
+                    aria-describedby="passwordHelpInline" 
+                    onChange = {handleChange}
+                    />
+                
+                <div className="col-auto">
+                    <span id="passwordHelpInline" class="form-text">
+                    Must be 8-20 characters long.
+                    </span>
                 </div>
 
                 <div className = "form-group text-left">
@@ -42,6 +70,7 @@ function RegisterationForm(){
                         className="form-control" 
                         id="confirmPassword" 
                         placeholder="Confirm Password"
+                        onChange = {handleChange}
                     />
                 </div>
 
@@ -49,12 +78,17 @@ function RegisterationForm(){
                 <button 
                     type="submit"
                     className="btn btn-primary"
+                    onClick = {handleSubmitChange}
                     >
                     Register
                 </button>
 
+                </div>
             </form>
+
+            
         </div>
+        
     );
 }
 
